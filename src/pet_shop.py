aef84be081ab3c9ba193a333539d69672df69808
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-11-19 11:51:06 +0000

    Added the increase_pets_sold function

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -12,3 +12,5 @@ def add_or_remove_cash(pet_shop, cash):
 def get_pets_sold(pet_shop):
     return pet_shop['admin']['pets_sold']
 
+def increase_pets_sold(pet_shop, sold):
+    pet_shop['admin']['pets_sold'] += sold
