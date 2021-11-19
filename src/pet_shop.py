906f6100e3c30ca8cae3465b2f7fcec5ae538c79
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-11-19 13:48:21 +0000

    Added the customer_can_afford_pet function

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -54,3 +54,8 @@ def get_customer_pet_count(customer):
 
 def add_pet_to_customer(customer, pet):
     customer['pets'].append(pet)
+
+def customer_can_afford_pet(customer, pet):
+    if get_customer_cash(customer) >= pet['price']:
+        return True
+    return False
