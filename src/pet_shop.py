7275fd9c56a601ecca2f26510500ffd92822d08c
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-11-19 11:53:47 +0000

    Added the get_stock_count function

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -14,3 +14,6 @@ def get_pets_sold(pet_shop):
 
 def increase_pets_sold(pet_shop, sold):
     pet_shop['admin']['pets_sold'] += sold
+
+def get_stock_count(pet_shop):
+    return len(pet_shop['pets'])
